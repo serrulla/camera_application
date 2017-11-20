4537a23b91d89e5c4ace2b85fe070de40922fba0
Eric Serrulla García <ericsg1@tr12.cat>
2017-11-20 10:41:42 +0100

    new js ok

---
+++ www/js/myjs.js
@@ -0,0 +1,15 @@
+function init(){
+
+    navigator.camera.getPicture(onSuccess, onFail, {quality: 58, destinationType: Camera.DestinationType.FILE_URI});
+
+function onSuccess(imageURI){
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+    }
+
+function onFail(message){
+    alert('Failed because: '+message);
+}
+
+
+}
